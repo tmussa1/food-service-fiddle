9a37e9829004101bd3e613feec2b2760a655ba17
Tofik Mussa <tofahub@gmail.com>
2019-11-11 00:29:38 -0500

    corrected file loc errors

--- src/main/java/cscie55/hw5/writer/MenuWriter.java
+++ src/main/java/cscie55/hw5/writer/MenuWriter.java
@@ -5,11 +5,11 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import cscie55.hw5.foodservice.Dish;
-import cscie55.hw5.foodservice.TakeOutShop;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.File;
 import java.io.IOException;
-import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -24,6 +24,8 @@ import java.util.List;
 
 public class MenuWriter {
 
+    private static final Logger LOGGER = LogManager.getLogger(MenuWriter.class.getName());
+
     /**
      *
      * @param menuFileName - a string used as file name, must include extension
@@ -33,10 +35,11 @@ public class MenuWriter {
     public static void publish(String menuFileName, List<Dish> menu) {
         ObjectMapper mapper = new ObjectMapper();
         mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
-        // as a default,menu.json is the file name
+        // as a default,
+        // menu.json is the file name
         String fileName = (menuFileName.equals(null)|| menuFileName.equals("") || menuFileName.equals(" ") ) ? "menu.json": menuFileName;
         Path path = Paths.get(System.getProperty("user.dir"), "src", "main", "resources", fileName);
-
+        LOGGER.info("Writing to file");
         File f = new File(path.toString());
             try {
                 if(f.exists())
